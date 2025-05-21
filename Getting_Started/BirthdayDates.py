fcab1e2d566c18d7bf9d57d751e23abfa901c7ec
Hamza14055 <michael.21smcs@gmail.com>
2025-05-21 16:12:56 +0500

    BirthdayDates

---
+++ Getting_Started/BirthdayDates.py
@@ -0,0 +1,7 @@
+_Hamza_Birthday = "4th March 2009"
+_Friend1_Birthday = "5th May 2008"
+_Friend2_Birthday = "6th June 2009"
+_Raj_Birthday = "9th September 2010"
+_Yahya_Birthday = "10th November 2008"
+print (_Hamza_Birthday,_Raj_Birthday,_Friend1_Birthday,_Friend2_Birthday,_Yahya_Birthday)
+
