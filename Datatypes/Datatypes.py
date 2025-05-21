633663fa754e1b451abb31baf558fcbf1e981723
Hamza14055 <michael.21smcs@gmail.com>
2025-05-21 16:49:46 +0500

    Datatypes

---
+++ Datatypes/Datatypes.py
@@ -0,0 +1,8 @@
+Number = 1234
+Name = "Hamza"
+My_Value = True
+My_Float = 0.34
+print ("Data Type of Number is " , type(Number),"\n")
+print ("Data Type of Number is " , type(Name),"\n")
+print ("Data Type of Number is " , type(My_Value),"\n")
+print ("Data Type of Number is " , type (My_Float),"\n")
