fc5f55f2872f487028c3e4ecc955712c1c42d0e8
Hamza14055 <michael.21smcs@gmail.com>
2025-05-21 17:02:04 +0500

    Typecasting

---
+++ Datatypes/Typecasting.py
@@ -0,0 +1,8 @@
+Number = 1234
+My_Float = 0.34
+print ("Before TypeCasting")
+print ("Data Type of Number is " , type(Number),"\n")
+print ("Data Type of My_Float is " , type (My_Float),"\n")
+print ("After TypeCasting")
+print ("Data Type of Number is " , type(float (Number)),float (Number),"\n")
+print ("Data Type of My_Float is " , type(int (My_Float)),int (My_Float),"\n")
