2ed4901c69f7ae987c5ddef0d24fb37d35e3f3e8
Hamza14055 <michael.21smcs@gmail.com>
2025-05-22 16:47:45 +0500

    Average

---
+++ Operators/Average.py
@@ -0,0 +1,8 @@
+tree_1 = 98
+tree_2 = 94
+tree_3 = 41
+tree_4 = 96
+tree_5 = 11
+Sum = (tree_1 + tree_2 + tree_3 + tree_4 + tree_5)
+average = (Sum/5)
+print ("The Average height of trees in jack's front yard is ", average)
