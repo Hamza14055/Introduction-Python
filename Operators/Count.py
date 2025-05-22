80936003336a550c0506bcc1df0439f372bc22cc
Hamza14055 <michael.21smcs@gmail.com>
2025-05-22 17:09:50 +0500

    Percentage

---
+++ Operators/Count.py
@@ -0,0 +1,8 @@
+Ammount = int (input("Enter the Ammount"))
+Note1 = Ammount//100 
+Note2 = (Ammount%100)//50
+Note3 = ((Ammount%100)%50)//10
+print ("The Count of 100 Notes is" , Note1 ,"The Count of 50 Notes is " , Note2 , "The Count of 10 Notes is", Note3 )
+
+
+
---
+++ Operators/Percentage.py
@@ -0,0 +1,8 @@
+Math = 40
+Science = 70
+Hindi = 50
+English = 60
+percentage = ((Math + Science + Hindi + English )/400) * 100
+print ("The Percenatge of Raj is",percentage)
+
+
