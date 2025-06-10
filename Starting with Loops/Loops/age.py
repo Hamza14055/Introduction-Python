5d449a27ffbb653d72947852a3e541b5a2e70632
Hamza14055 <michael.21smcs@gmail.com>
2025-06-10 11:39:40 +0500

    Nested if

---
+++ Starting with Loops/Loops/age.py
@@ -0,0 +1,8 @@
+age = int(input("What is your age ? "))
+if age > 10 :
+    if age < 20 :
+        print("You are allowed to join ")
+    else:
+        print("You are not allowed to join ")
+else :
+    print("You are not allowed to join ")
