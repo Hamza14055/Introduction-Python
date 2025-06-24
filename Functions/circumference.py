56da439034df7e4dc96a0cbbab9f58aa72cbd0d9
Hamza14055 <michael.21smcs@gmail.com>
2025-06-24 14:22:44 +0500

    arguements

--- Functions/circumference.py
+++ Functions/circumference.py
@@ -1,7 +1,12 @@
-def circumference(Radius):
-    circ = 2*3.14*Radius
-    print(circ)
-Choice = int(input("Enter the radius : "))
-circumference(Choice)
+def circumference():
+    if choice_2.lower()=="radius":
+     circ = 2*3.14*Choice
+     print(circ)
+    elif choice_2.lower()=="diameter":
+       circ = 3.14*Choice
+       print(circ)
+Choice = int(input("Enter the value : "))
+choice_2 = input("is it a radius or a diameter : ")
+circumference()
 
 
